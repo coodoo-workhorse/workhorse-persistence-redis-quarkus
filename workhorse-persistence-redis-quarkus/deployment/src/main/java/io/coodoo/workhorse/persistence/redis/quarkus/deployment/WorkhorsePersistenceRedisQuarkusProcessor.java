603859beceb0401097e3e521979907fac4e6d6cf
Yvaris Djouda <yvaris.djouda@coodoo.de>
2021-10-19 14:39:33 +0200

    first commit

---
+++ workhorse-persistence-redis-quarkus/deployment/src/main/java/io/coodoo/workhorse/persistence/redis/quarkus/deployment/WorkhorsePersistenceRedisQuarkusProcessor.java
@@ -0,0 +1,45 @@
+package io.coodoo.workhorse.persistence.redis.quarkus.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
+import java.lang.reflect.Modifier;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.processor.DotNames;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+
+class WorkhorsePersistenceRedisQuarkusProcessor {
+
+    private static final String FEATURE = "workhorse-persistence-redis-quarkus";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    IndexDependencyBuildItem indexExternalDependency() {
+        return new IndexDependencyBuildItem("io.coodoo", "workhorse-persistence-redis");
+    }
+
+    @BuildStep
+    public void declareWorkhorseAsBean(CombinedIndexBuildItem index,
+            BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
+
+        // All cdi-Beans are here registered to be available in quarkus application
+        // In the future we can select beans that we want to expose to an quarkus
+        // application.
+        // I.e register WorkhorseService but not WorkhorseController.
+        List<String> workhorseBeans = index.getIndex().getKnownClasses().stream()
+                .filter(ci -> !Modifier.isAbstract(ci.flags())).map(ci -> ci.name().toString())
+                .filter(c -> c.startsWith("io.coodoo.workhorse.persistence.redis.")).collect(Collectors.toList());
+
+        additionalBeans.produce(new AdditionalBeanBuildItem.Builder().addBeanClasses(workhorseBeans).setUnremovable()
+                .setDefaultScope(DotNames.DEFAULT_BEAN).build());
+    }
+}
---
+++ workhorse-persistence-redis-quarkus/deployment/target/generated-sources/annotations/io/coodoo/workhorse/persistence/redis/quarkus/deployment/WorkhorsePersistenceRedisQuarkusProcessor$$accessor.java
@@ -0,0 +1,7 @@
+package io.coodoo.workhorse.persistence.redis.quarkus.deployment;
+public final class WorkhorsePersistenceRedisQuarkusProcessor$$accessor {
+    private WorkhorsePersistenceRedisQuarkusProcessor$$accessor() {}
+    public static Object construct() {
+        return new WorkhorsePersistenceRedisQuarkusProcessor();
+    }
+}
