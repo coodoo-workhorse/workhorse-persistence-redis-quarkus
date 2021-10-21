03184d2e705ad29de413bdd358bf3170a9ba7862
Yvaris Djouda <yvaris.djouda@coodoo.de>
2021-10-21 15:21:23 +0000

    generated files deleted

--- deployment/target/generated-sources/annotations/io/coodoo/workhorse/persistence/redis/quarkus/deployment/WorkhorsePersistenceRedisQuarkusProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package io.coodoo.workhorse.persistence.redis.quarkus.deployment;
-public final class WorkhorsePersistenceRedisQuarkusProcessor$$accessor {
-    private WorkhorsePersistenceRedisQuarkusProcessor$$accessor() {}
-    public static Object construct() {
-        return new WorkhorsePersistenceRedisQuarkusProcessor();
-    }
-}
